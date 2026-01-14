5a2b6c81ad9cba4487cac2369ed9e6015a06af5b
David Moura <davidmoura@dev.otimize.me>
2026-01-14 18:17:44 -0300

    feat: initial game configs

---
+++ src/config/resources.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'gold' => [
+        'name' => 'gold',
+        'description' => 'Universal currency for trade.',
+        'tradeable' => false
+    ],
+    'food' => [
+        'name' => 'Food',
+        'description' => 'Keeps your population fed and is required for some upgrades.',
+        'tradeable' => true
+    ],
+    'wood' => [
+        'name' => 'Wood',
+        'description' => 'Basic building material.',
+        'tradeable' => true
+    ],
+    'stone' => [
+        'name' => 'Stone',
+        'description' => 'Used for strong structures and upgrades.',
+        'tradeable' => true
+    ],
+];
